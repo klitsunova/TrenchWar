36713335f8711fe7f1861d693d3111f23e66f560
unknown <azyavchikov.alex@gmail.com>
2022-04-26 14:39:42 +0300

    map_generator_added

---
+++ MapGenerator/map_generator.cpp
@@ -0,0 +1,374 @@
+#include "map_generator.h"
+
+MapGenerator::MapGenerator(QWidget* parent)
+    : QWidget(parent),
+      original_picture_shell_(new ImageSHell(this)),
+      changed_picture_shell_(new ImageSHell(this)),
+      using_colors_shell_(new ImageSHell(this)),
+      color_shell_(new ImageSHell(this)),
+      draw_button_(new QPushButton("Draw", this)),
+      change_button_(new QPushButton("Change colors", this)),
+      restore_button_(new QPushButton("Restore color changes", this)),
+      save_button_(new QPushButton("Save color changes", this)),
+      add_color_button_(new QPushButton("<--", this)),
+      width_(new QLineEdit("500", this)),
+      height_(new QLineEdit("500", this)),
+      speed_characteristic_(new QLineEdit("0", this)),
+      speed_characteristic_label_(new QLabel("Speed characteristic",
+                                             this)) {
+  // temporary code
+  // we need to make possible to set the way of picture
+  original_picture_shell_->SetPixmap("../map1.png");
+  buffer_picture_.load("../map1.png");
+
+  SetAdditionalColorVisible(false);
+
+  save_button_->setVisible(false);
+
+  using_colors_.emplace_back(Landscape(QColor(255, 255, 255), 0));
+
+  connect(draw_button_,
+          &QPushButton::clicked,
+          this,
+          &MapGenerator::DrawButtonClicked);
+  connect(add_color_button_,
+          &QPushButton::clicked,
+          this,
+          &MapGenerator::AddColorButtonClicked);
+  connect(change_button_,
+          &QPushButton::clicked,
+          this,
+          &MapGenerator::ChangeButtonClicked);
+  connect(restore_button_,
+          &QPushButton::clicked,
+          this,
+          &MapGenerator::RestoreButtonClicked);
+  connect(save_button_,
+          &QPushButton::clicked,
+          this,
+          &MapGenerator::SaveButtonClicked);
+
+  DrawButtonClicked();
+  DrawUsingColors();
+
+  ManageLayout();
+}
+
+void MapGenerator::ManageLayout() {
+  auto layout1 = new QHBoxLayout();
+  layout1->addWidget(original_picture_shell_, 10);
+  layout1->addStretch(1);
+  layout1->addWidget(changed_picture_shell_, 10);
+
+  auto layout2 = new QVBoxLayout();
+  layout2->addStretch(1);
+  layout2->addLayout(layout1, 30);
+  layout2->addStretch(1);
+
+  auto layout3 = new QHBoxLayout();
+  layout3->addWidget(using_colors_shell_, 5);
+  layout3->addWidget(add_color_button_);
+  layout3->addWidget(color_shell_, 2);
+
+  auto layout4 = new QVBoxLayout();
+  layout4->addStretch(1);
+  layout4->addWidget(speed_characteristic_label_);
+  layout4->addWidget(speed_characteristic_);
+  layout4->addStretch(1);
+
+  layout3->addLayout(layout4);
+
+  layout2->addLayout(layout3, 3);
+  layout2->addStretch(1);
+
+  auto layout5 = new QVBoxLayout();
+  layout5->addStretch(1);
+  auto label1 = new QLabel("Width", this);
+  layout5->addWidget(label1, 0);
+  layout5->addWidget(width_, 0);
+  auto label2 = new QLabel("Height", this);
+  layout5->addWidget(label2, 0);
+  layout5->addWidget(height_, 0);
+  layout5->addStretch(1);
+  layout5->addWidget(draw_button_, 0);
+  layout5->addWidget(change_button_, 0);
+  layout5->addWidget(restore_button_, 0);
+  layout5->addWidget(save_button_, 0);
+  layout5->addStretch(1);
+
+  auto layout6 = new QHBoxLayout(this);
+  layout6->addStretch(1);
+  layout6->addLayout(layout2, 30);
+  layout6->addStretch(1);
+  layout6->addLayout(layout5, 0);
+  layout6->addStretch(1);
+}
+
+void MapGenerator::ConvertImageToArray(const QSize& size) {
+  assert(!buffer_picture_.isNull());
+
+  if (map_.size() > 0 && size.height() == map_.size()
+      && size.width() == map_[0].size()) {
+    return;
+  }
+
+  using_original_colors = true;
+  save_button_->setVisible(false);
+
+  map_.resize(size.height());
+  for (int i = 0; i < map_.size(); ++i) {
+    map_[i].resize(size.width());
+  }
+
+  int window_width = buffer_picture_.width() - 1;
+  int window_height = buffer_picture_.height() - 1;
+  for (int i = 0; i < size.height(); ++i) {
+    for (int j = 0; j < size.width(); ++j) {
+      int x_top = (j * window_width) / size.width();
+      int y_top = (i * window_height) / size.height();
+
+      int x_bottom = ((j + 1) * window_width) / size.width();
+      int y_bottom = ((i + 1) * window_height) / size.height();
+
+      int points_number = 0;
+      int64_t r = 0, g = 0, b = 0;
+
+      for (int x = x_top; x <= x_bottom; ++x) {
+        for (int y = y_top; y <= y_bottom; ++y) {
+          ++points_number;
+          r += qRed(buffer_picture_.pixel(x, y));
+          g += qGreen(buffer_picture_.pixel(x, y));
+          b += qBlue(buffer_picture_.pixel(x, y));
+        }
+      }
+      map_[i][j].original_color = QColor(r / points_number,
+                                         g / points_number,
+                                         b / points_number).rgb();
+      map_[i][j].using_color = -1;
+    }
+  }
+}
+
+void MapGenerator::DrawChangedPicture() {
+  assert(!map_.empty());
+  QPainter painter(&changed_picture_shell_->picture_);
+  int map_width, map_height;
+  int window_width = painter.window().width() - 1;
+  int window_height = painter.window().height() - 1;
+  map_height = map_.size();
+  map_width = map_[0].size();
+  QColor color;
+  for (int i = 0; i < map_.size(); ++i) {
+    for (int j = 0; j < map_[i].size(); ++j) {
+      if (using_original_colors) {
+        color = map_[i][j].original_color;
+      } else {
+        assert(map_[i][j].using_color >= 0
+                   && map_[i][j].using_color < using_colors_.size());
+        color = using_colors_[map_[i][j].using_color].color;
+      }
+      painter.setBrush(color);
+      painter.setPen(QPen(color, 0));
+
+      QPoint top = QPoint((j * window_width) / map_width,
+                          (i * window_height) / map_height);
+      QPoint
+          bottom = QPoint(((j + 1) * window_width) / map_width,
+                          ((i + 1) * window_height) / map_height);
+
+      painter.drawRect(QRect(top, bottom));
+    }
+  }
+  changed_picture_shell_->update();
+}
+
+void MapGenerator::DrawUsingColors() {
+  assert(!using_colors_.empty());
+  QPainter painter(&using_colors_shell_->picture_);
+  int window_width = painter.window().width() - 1;
+  int window_height = painter.window().height() - 1;
+  for (int i = 0; i < using_colors_.size(); ++i) {
+    painter.setBrush(using_colors_[i].color);
+    painter.setPen(QPen(using_colors_[i].color, 0));
+
+    QPoint top = QPoint((i * window_width) / using_colors_.size(),
+                        0);
+    QPoint
+        bottom = QPoint(((i + 1) * window_width) / using_colors_.size(),
+                        window_height);
+
+    painter.drawRect(QRect(top, bottom));
+  }
+  using_colors_shell_->update();
+}
+
+void MapGenerator::ChangeMap() {
+  int original_red, delta_red;
+  int original_blue, delta_blue;
+  int original_green, delta_green;
+
+  for (int i = 0; i < map_.size(); ++i) {
+    for (int j = 0; j < map_[i].size(); ++j) {
+      original_red = map_[i][j].original_color.red();
+      original_green = map_[i][j].original_color.green();
+      original_blue = map_[i][j].original_color.blue();
+
+      int64_t precision = -1;
+      int64_t p;
+      for (int k = 0; k < using_colors_.size(); ++k) {
+        delta_red = using_colors_[k].color.red() - original_red;
+        delta_green = using_colors_[k].color.green() - original_green;
+        delta_blue = using_colors_[k].color.blue() - original_blue;
+        p = delta_red * delta_red
+            + delta_green * delta_green
+            + delta_blue * delta_blue;
+        if (precision < 0 || p < precision) {
+          map_[i][j].using_color = k;
+          precision = p;
+        }
+      }
+    }
+  }
+}
+
+void MapGenerator::mouseDoubleClickEvent(QMouseEvent* event) {
+  int x = event->pos().x();
+  int y = event->pos().y();
+  if (x >= original_picture_shell_->x() &&
+      y >= original_picture_shell_->y() &&
+      x <= (original_picture_shell_->x() +
+          original_picture_shell_->width()) &&
+      y <= (original_picture_shell_->y() +
+          original_picture_shell_->height())) {
+    x -= original_picture_shell_->x();
+    y -= original_picture_shell_->y();
+
+    SetAdditionalColorVisible(true);
+
+    QPainter painter(&color_shell_->picture_);
+    painter.save();
+
+    int i = (map_.size() * y) / (original_picture_shell_->height());
+    int j = (map_[i].size() * x) / (original_picture_shell_->width());
+
+    color_ = map_[i][j].original_color;
+    painter.setBrush(color_);
+    painter.setPen(QPen(color_, 0));
+    painter.drawRect(QRect(0,
+                           0,
+                           painter.window().width(),
+                           painter.window().height()));
+    color_shell_->update();
+    painter.restore();
+  } else if (x >= using_colors_shell_->x() &&
+      y >= using_colors_shell_->y() &&
+      x <= (using_colors_shell_->x() +
+          using_colors_shell_->width()) &&
+      y <= (using_colors_shell_->y() +
+          using_colors_shell_->height())) {
+    if (using_colors_.size() == 1) {
+      using_colors_[0].color = Qt::white;
+      using_colors_[0].speed_characteristic = 0;
+      return;
+    }
+    x -= using_colors_shell_->x();
+    int i = (using_colors_.size() * x) / (using_colors_shell_->width());
+    using_colors_.erase(using_colors_.begin() + i);
+    DrawUsingColors();
+  }
+}
+
+void MapGenerator::DrawButtonClicked() {
+  ConvertImageToArray(QSize(width_->text().toInt(),
+                            height_->text().toInt()));
+  DrawChangedPicture();
+}
+
+void MapGenerator::AddColorButtonClicked() {
+  using_colors_.emplace_back(color_,
+                             speed_characteristic_->text().toInt());
+  DrawUsingColors();
+
+  SetAdditionalColorVisible(false);
+}
+
+void MapGenerator::ChangeButtonClicked() {
+  ConvertImageToArray(QSize(width_->text().toInt(),
+                            height_->text().toInt()));
+  ChangeMap();
+  using_original_colors = false;
+  DrawChangedPicture();
+  save_button_->setVisible(true);
+}
+
+void MapGenerator::RestoreButtonClicked() {
+  using_original_colors = true;
+  DrawChangedPicture();
+  save_button_->setVisible(false);
+}
+
+void MapGenerator::SetAdditionalColorVisible(bool visible) {
+  color_shell_->setVisible(visible);
+  add_color_button_->setVisible(visible);
+  speed_characteristic_->setVisible(visible);
+  speed_characteristic_label_->setVisible(visible);
+}
+
+void MapGenerator::SaveButtonClicked() {
+  assert(!using_original_colors);
+  assert(map_.size() > 0 && map_[0].size() > 0);
+  // QFileDialog dialog(this);
+  // dialog.open();
+  // QStringList
+  //     selected_files = dialog.selectedFiles();
+  // if (selected_files.empty()) {
+  //   return;
+  // }
+  // QTextStream fout(*selected_files.begin());
+  std::ofstream fout("map.txt");
+  fout << using_colors_.size() << "\n";
+  for (int i = 0; i < using_colors_.size(); ++i) {
+    fout << using_colors_[i].color.rgb() << " "
+         << using_colors_[i].speed_characteristic << "\n";
+  }
+  fout << "\n" << map_.size() << " " << map_[0].size() << "\n";
+  for (int i = 0; i < map_.size(); ++i) {
+    for (int j = 0; j < map_[i].size(); ++j) {
+      fout << map_[i][j].using_color << " ";
+    }
+    fout << "\n";
+  }
+  fout.close();
+}
+
+MapGenerator::Landscape::Landscape(const QColor& color,
+                                   int speed_characteristic) {
+  this->color = color;
+  this->speed_characteristic = speed_characteristic;
+}
+
+MapGenerator::ImageSHell::ImageSHell(QWidget* parent)
+    : QWidget(parent) {
+  picture_ = QPixmap(QSize(1000, 1000));
+}
+
+void MapGenerator::ImageSHell::SetPixmap(const QPixmap& picture) {
+  picture_ = picture;
+  this->update();
+}
+
+void MapGenerator::ImageSHell::SetPixmap(const QString& path) {
+  picture_.load(path);
+  this->update();
+}
+
+void MapGenerator::ImageSHell::paintEvent(QPaintEvent*) {
+  if (picture_.isNull()) {
+    return;
+  }
+  QPainter painter(this);
+  painter.save();
+  painter.drawPixmap(QRect(0, 0, this->width(), this->height()),
+                     picture_);
+  painter.restore();
+}
---
+++ MapGenerator/map_generator.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <QApplication>
+#include <QPainter>
+#include <QPushButton>
+#include <QImage>
+#include <QLabel>
+#include <QLineEdit>
+#include <QWidget>
+#include <QMouseEvent>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QColor>
+#include <QString>
+#include <QFileDialog>
+#include <QStringList>
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+
+class MapGenerator : public QWidget {
+ Q_OBJECT;
+ private:
+  struct ladscape;
+  class ImageShell;
+
+ public:
+  MapGenerator(QWidget* parent = nullptr);
+
+  void mouseDoubleClickEvent(QMouseEvent*) override;
+
+ private:
+  struct ChangingColor {
+    int using_color{0};
+    QColor original_color{0xffffff};
+  };
+
+  struct Landscape {
+    Landscape(const QColor&, int);
+    QColor color{0xffffff};
+    int speed_characteristic{0};
+  };
+
+  class ImageSHell : public QWidget {
+    void SaveButtonClicked();
+
+   public:
+    explicit ImageSHell(QWidget* parent = nullptr);
+
+    void SetPixmap(const QPixmap&);
+    void SetPixmap(const QString&);
+
+    void paintEvent(QPaintEvent*) override;
+
+   private:
+    friend class MapGenerator;
+    QPixmap picture_;
+  };
+
+  void ManageLayout();
+
+  void SetAdditionalColorVisible(bool);
+
+  void DrawChangedPicture();
+  void DrawUsingColors();
+
+  void ConvertImageToArray(const QSize&);
+
+  void ChangeMap();
+
+  void DrawButtonClicked();
+  void AddColorButtonClicked();
+  void ChangeButtonClicked();
+  void RestoreButtonClicked();
+  void SaveButtonClicked();
+
+  std::vector<std::vector<ChangingColor>> map_;
+  bool using_original_colors{true};
+  std::vector<Landscape> using_colors_;
+  QImage buffer_picture_;
+  ImageSHell* original_picture_shell_;
+  ImageSHell* changed_picture_shell_;
+  ImageSHell* using_colors_shell_;
+  ImageSHell* color_shell_;
+  QColor color_;
+  QPushButton* draw_button_;
+  QPushButton* change_button_;
+  QPushButton* restore_button_;
+  QPushButton* save_button_;
+  QPushButton* add_color_button_;
+  QLineEdit* width_;
+  QLineEdit* height_;
+  QLineEdit* speed_characteristic_;
+  QLabel* speed_characteristic_label_;
+};
