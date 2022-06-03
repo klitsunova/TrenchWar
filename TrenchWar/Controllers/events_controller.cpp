7dd25a3b6c6d69eff7c0e05f125b8ad8ec5ea10c
Dmitry <dmitryrub16.01@gmail.com>
2022-06-03 12:29:19 +0300

    Show store After starting of the game store_viewbecomes unvivible

--- TrenchWar/Controllers/events_controller.cpp
+++ TrenchWar/Controllers/events_controller.cpp
@@ -77,6 +77,7 @@ void EventsController::HideGame() {
 void EventsController::Start(BuyMode mode) {
   CancelPurchase(mode);
   view_->Start();
+  view_->GetStore()->setVisible(false);
   game_stage = Stage::kActive;
   StartTimer();
 }
